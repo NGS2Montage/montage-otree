eedadb7e3314a9d1f70d737f115ffd3cd58e883d
bjgoode <bjgoode@vt.edu>
2017-04-23 22:43:56 -0400

    add wait live wait updates

--- core/consumers.py
+++ core/consumers.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 from channels import Group
 from channels.auth import channel_session_user
 
+from otree.models.participant import Participant
 # from .models import ChatMessage
 
 '''
@@ -31,3 +32,29 @@ def chat_consumer(message):
     })
 
 '''
+
+group_name = "waitPageCount"
+
+def connect_waitPage_count(message,params):
+    
+    participant_code = params.split(',')[0]
+    participant = Participant.objects.filter(code=participant_code).first()
+    all_participants = participant.session.participant_set.all()
+
+    n_participants =len(all_participants)
+    present_participants = [p.is_on_wait_page for p in all_participants]
+    n_remaining = n_participants - sum(present_participants)
+
+    group = Group(group_name)
+    group.add(message.reply_channel)
+
+    group.send(
+        {'text': json.dumps(
+            {'N': n_participants, 'remaining': n_remaining}
+        )}
+    )
+
+def disconnect_waitPage_count(message):
+
+    group = Group(group_name)
+    group.discard(message.reply_channel)
--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,16 @@
 
+from channels.routing import route
 from otree.channels.routing import channel_routing
 from anagrams.routing import anagrams_routing
 from ultimatum.routing import ultimatum_routing
+from .consumers import connect_waitPage_count, disconnect_waitPage_count
 
 channel_routing += anagrams_routing + ultimatum_routing
+channel_routing += [
+    route(
+        'websocket.connect', connect_waitPage_count,
+        path=r'^/playerCount/(?P<params>[\w,]+)/$'),
+    route(
+        'websocket.disconnect', disconnect_waitPage_count,
+        path=r'^/playerCount'),
+]
