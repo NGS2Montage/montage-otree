7c52582dd8b46c73b81450b8583c8d588f03c34c
Nathan Self <nwself@cs.vt.edu>
2017-07-10 16:45:35 -0400

    Change 'predictions' to 'responses' in ruse wait page (favorite color, etc. are not predictions)

--- ruse/views.py
+++ ruse/views.py
@@ -46,9 +46,9 @@ class Introduction(Page):
 
 class WaitPage(WaitPage):
     is_debug = False
-    title_text = "Processing Predictions"
+    title_text = "Processing Responses"
     body_text = """Thank you, please wait while the rest of the team submits their
-    predictions."""
+    responses."""
 
     def is_displayed(self):
         if self.participant.vars['consent'] and \
